c69dd5fb2a3683c1ab3895565c8482d8df20400f
Graham Reed <greed@7deadly.org>
2020-09-03 20:04:55 +0100

    Switch to flat mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -983,7 +983,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -13, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -51, -12, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
