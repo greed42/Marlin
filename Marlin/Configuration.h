1c3266f45b4a3ebe000dedcf7092c31d4b047237
Graham Reed <greed@7deadly.org>
2021-01-04 18:04:55 +0000

    Switch to Hero Me compact wing for BLTouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -980,7 +980,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -51, -12, -1.90 }
+#define NOZZLE_TO_PROBE_OFFSET { -39, 7, -2.15 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
